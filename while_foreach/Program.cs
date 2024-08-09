fc4c90a0f25dce9eaee372930f47f9f625eabef9
Muzaffer Sünbül <101253600+muzaffersnbl@users.noreply.github.com>
2024-08-09 10:55:09 +0300

    Add files via upload

---
+++ while_foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace while_foreach
+
+{
+
+    class Program 
+
+    {
+        static void Main(string []  args)
+        {
+            // Console.WriteLine("Bir sayı giriniz: ");
+            // int sayi = int.Parse(Console.ReadLine());
+            // int sayac = 1;
+            // int toplam  = 0;
+            // while (sayac <= sayi)
+            // {
+            // toplam += sayac;
+            // sayac ++;
+
+            // }
+            // Console.WriteLine(toplam/sayi);
+            // char karakter = 'a';
+            // while(karakter  < 'n')
+            // {
+            //     Console.Write(karakter);
+            //     karakter ++;
+            // }
+
+            string[]  arabalar = {"BMW" , "Ford" , "Nisaan","Honda" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+
+            } 
+        }
+    }
+}
